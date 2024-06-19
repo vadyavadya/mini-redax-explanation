1a14472abaa6bc014ed72f9db5a5e2caeaf7c046
Vadim Sergeev <vadevrazserg@gmail.com>
2024-06-19 16:32:22 +0700

    added block

--- src/components/content/Content.tsx
+++ src/components/content/Content.tsx
@@ -1,19 +1,26 @@
 // import { useSelector } from "react-redux";
 import { useAppSelector } from "../../app/hooks"
 import { selectContent } from "./contentSlice"
+import style from './style.module.css'
 
 export default function Content() {
-  // получаем наже сотояние через хук useSelector() тут из за ts используется useAppSelector
+  // получаем наше сотояние через хук useSelector() тут из за ts используется useAppSelector
   const content = useAppSelector(selectContent);
-
   // так тоже работает
   // const content = useSelector(selectContent);
 
+  // задаем стили в зависимости от состояния
+  let styleBlock = style.redBlock;
+  if (!content) {
+    styleBlock += ' ' + style.hide;
+  }
+
   return (
     <div className="container">
-    <div>
-      Значение store content: {content ? "true" : "fasle"}
-    </div>
+      <div>
+        Значение store content: {content ? "true" : "fasle"}
+      </div>
+      <div className={styleBlock}></div>
     </div>
   )
 }
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,15 +1,18 @@
 // import { useDispatch } from "react-redux";
-import { useAppDispatch } from "../../app/hooks";
-import { toggleVisible } from "../content/contentSlice";
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
+import { selectContent, toggleVisible } from "../content/contentSlice";
 import style from "./style.module.css";
 
 // Заголовок где по нажатию кнопки происходит изменения т.к. тут состояние не нужно
 // мы объявляем только диспатч(через него мы говорим стору что надо поменять состояние)
 export default function Header() {
   const dispatch = useAppDispatch();
-
   // для js без ts используй это
   // const dispatch = useDispatch();
+  
+  // получение состояния для подписи в кнопке
+  const content = useAppSelector(selectContent);
+  let textButton = content ? 'Скрыть': 'Показать' 
 
   return (
     <header>
@@ -17,7 +20,7 @@ export default function Header() {
         <div className={style["d-flex"]}>
           <div className="">Заголовок</div>
           {/* тут мы по нажатию, обращаемся к стору через диспачь и говорим что делать какой акшин нужно выполнить */}
-          <button onClick={()=> dispatch(toggleVisible())}>Скрыть</button>
+          <button onClick={()=> dispatch(toggleVisible())}>{textButton}</button>
         </div>
       </div>
     </header>
